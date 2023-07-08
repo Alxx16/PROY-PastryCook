f6d7fb58813bb30bba6b2965e28f0f7b2d761fd5
Christopher Rodriguez <ggxirrus@gmail.com>
2023-07-08 13:54:22 -0500

    First Commit

---
+++ Express/App.js
@@ -0,0 +1,11 @@
+const {pokemon} = require("./pokemon.js")
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+
+app.listen(PORT, () => {console.log(`Servidor Iniciando en el Puerto# ${PORT}`)})
