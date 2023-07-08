4c1afaabd7df0e6475638ac4803a27c0b884c46b
Christopher Rodriguez <ggxirrus@gmail.com>
2023-07-08 15:06:04 -0500

    Nueva Carpeta

---
+++ pastrycook1/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-violet-100 h-screen" >
+    <BoxNab/>
+    <BoxBody/>
+
+    <BoxFoot/>
+  </div>
+   
+</template>
+
+<script>
+import BoxNab from './components/BoxNav.vue';
+import BoxFoot from './components/BoxFoot.vue';
+import BoxBody from './components/BoxBody.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BoxNab,
+    BoxFoot,
+    BoxBody
+  }
+}
+</script>
+
---
+++ pastrycook1/src/components/BoxNav.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav id="Navbar" class="flex items-center justify-between bg-rose-300 p-4">
+    <a href="../assets/img/logo.png" class="flex items-center text-white">
+      <img src="../assets/img/logo.png" alt="logo" class="h-24 ">
+      <!-- <span class="font-semibold text-xl tracking-tight"></span> -->
+    </a>
+    <button class="bg-amber-300 hover:amber-500 text-white font-semibold py-2 px-4 rounded hidden md:block">
+      Acceder
+    </button>
+    <button class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded md:hidden">
+      <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+        <path
+          fill-rule="evenodd"
+          d="M3 4a1 1 0 100 2h14a1 1 0 100-2H3zm0 5a1 1 0 100 2h14a1 1 0 100-2H3zm0 5a1 1 0 100 2h14a1 1 0 100-2H3z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </button>
+  </nav>
+</template>
+
+
+
+<script>
+
+export default {
+  
+}
+
+</script>
