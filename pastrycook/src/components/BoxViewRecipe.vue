c6f873b89ad8ddbb5d0fe6821184c238bd691aee
Xirrus5 <104747827+Xirrus5@users.noreply.github.com>
2023-07-24 12:15:52 -0500

    Plan c

--- pastrycook/src/components/BoxViewRecipe.vue
+++ pastrycook/src/components/BoxViewRecipe.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+  <div class="flex flex-col items-center justify-center min-h-screen py-2">
       <h3>{{ titulo }}</h3>
       <span>{{ id_r }}</span>
       <p>{{ username }}</p>
