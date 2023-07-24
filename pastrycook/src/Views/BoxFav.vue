6451502016bc8e8ad612a128941bed0edbd5790d
JairFloresM <jairfm01@gmail.com>
2023-07-24 02:34:31 -0500

    se creo la pagina de favoritos junto con si paginacion

--- pastrycook/src/Views/BoxFav.vue
+++ pastrycook/src/Views/BoxFav.vue
@@ -1,53 +1,19 @@
 <template>
  <!-- Recetas -->
   <div>
-  <div class="flex min-h-full flex-1 flex-col justify-center pb-12 padding-top: 0px;">
-        <div class="mt-0"> 
-            <h3 class="uppercase p-5 bg-violet-200 w-full text-left text-2xl  font-sans font-light mb-8">Recetas</h3> 
-        </div>
-    <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12 padding-top: 0px;">
-  <div class="-m-1 flex flex-wrap md:-m-2">
-     <!-- Recetas -->
-    <div class="flex w-1/3 flex-wrap relative">
-      <div class="w-full p-1 md:p-2 relative">
-        <img alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="../../public/img/Pastel.png" />
-
-          <p class="position: absolute; text-base font-semibold text-gray-900">La mejor manera de entrar en calor en un día frío</p>
-      </div>
-    </div>
-    <div class="flex w-1/3 flex-wrap relative ">
-      <div class="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="../../public/img/Pastel.png" />
-
-          <p class=" font-semibold pt-15 text-gray-900">La mejor manera de entrar en calor en un día frío</p>
-      </div>
-    </div>
-    <div class="flex w-1/3 flex-wrap">
-      <div class="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="../../public/img/Pastel.png" />
-          
-          <p class="text-base font-semibold text-gray-900">La mejor manera de entrar en calor en un día frío</p>
-      </div>
-    </div>
-  
+    <BoxRecetas :Pastry="recetas"/>
   </div>
-</div>
-</div>
-
-</div>
-    </template>
+</template>
 
 <script>
+import BoxRecetas from './BoxRecetas.vue';
+
+
     export default{
         namePages: 'Favorite',
+        components:{
+          BoxRecetas
+        },
         data(){
             return{
                 imgFavorite:[
@@ -65,6 +31,153 @@
             }
             
           ],
+          recetas : [
+        {
+          id:1,
+          descripcion:"La mejor manera de refrescarse en un día caluroso",
+          titulo:"Recetas frias",
+          autor:"Pastry Cook",
+          costo:"$ 100",
+          ingredientes:"una polla caliente",
+          categoria:"Frias",
+          nivel:"Principiante",
+
+        },
+        {
+          id:2,
+          descripcion:"La mejor manera de entrar en calor en un día frío",
+          titulo:"Receta calientes",
+          autor:"cocinero 1",
+          costo:"$ 200",
+          ingredientes:"una polla fria",
+          categoria:"Calientes",
+          nivel:"Avanzado", 
+        },
+        {
+          id:3,
+          descripcion:"aslkdnasd",
+          titulo:"asdasdaqsdas",
+          autor:"coasdasd",
+          costo:"$ 20220",
+          ingredientes:"uasdas",
+          categoria:"Csadasd",
+          nivel:"asdasd", 
+        },
+        {
+          id:1,
+          descripcion:"La mejor manera de refrescarse en un día caluroso",
+          titulo:"Recetas frias",
+          autor:"Pastry Cook",
+          costo:"$ 100",
+          ingredientes:"una polla caliente",
+          categoria:"Frias",
+          nivel:"Principiante",
+
+        },
+        {
+          id:2,
+          descripcion:"La mejor manera de entrar en calor en un día frío",
+          titulo:"Receta calientes",
+          autor:"cocinero 1",
+          costo:"$ 200",
+          ingredientes:"una polla fria",
+          categoria:"Calientes",
+          nivel:"Avanzado", 
+        },
+        {
+          id:3,
+          descripcion:"aslkdnasd",
+          titulo:"asdasdaqsdas",
+          autor:"coasdasd",
+          costo:"$ 20220",
+          ingredientes:"uasdas",
+          categoria:"Csadasd",
+          nivel:"asdasd", 
+        },
+        {
+          id:1,
+          descripcion:"La mejor manera de refrescarse en un día caluroso",
+          titulo:"Recetas frias",
+          autor:"Pastry Cook",
+          costo:"$ 100",
+          ingredientes:"una polla caliente",
+          categoria:"Frias",
+          nivel:"Principiante",
+
+        },
+        {
+          id:2,
+          descripcion:"La mejor manera de entrar en calor en un día frío",
+          titulo:"Receta calientes",
+          autor:"cocinero 1",
+          costo:"$ 200",
+          ingredientes:"una polla fria",
+          categoria:"Calientes",
+          nivel:"Avanzado", 
+        },
+        {
+          id:3,
+          descripcion:"aslkdnasd",
+          titulo:"asdasdaqsdsssssssas",
+          autor:"coasdasd",
+          costo:"$ 20220",
+          ingredientes:"uasdas",
+          categoria:"Csadasd",
+          nivel:"asdasd", 
+        },
+        {
+          id:1,
+          descripcion:"La mejor manera de refrescarse en un día caluroso",
+          titulo:"Recetas friassssssss",
+          autor:"Pastry Cook",
+          costo:"$ 100",
+          ingredientes:"una polla caliente",
+          categoria:"Frias",
+          nivel:"Principiante",
+
+        },
+        {
+          id:2,
+          descripcion:"La mejor manera de entrar en calor en un día frío",
+          titulo:"Receta calientesssssss",
+          autor:"cocinero 1",
+          costo:"$ 200",
+          ingredientes:"una polla fria",
+          categoria:"Calientes",
+          nivel:"Avanzado", 
+        },
+        {
+          id:3,
+          descripcion:"aslkdnasd",
+          titulo:"asdasdaqsdassssssss",
+          autor:"coasdasd",
+          costo:"$ 20220",
+          ingredientes:"uasdas",
+          categoria:"Csadasd",
+          nivel:"asdasd", 
+        },
+        {
+          id:2,
+          descripcion:"La mejor manera de entrar en calor en un día frío",
+          titulo:"Receta calientes",
+          autor:"cocinero 1",
+          costo:"$ 200",
+          ingredientes:"una polla fria",
+          categoria:"Calientes",
+          nivel:"Avanzado", 
+        },
+        {
+          id:3,
+          descripcion:"aslkdnasd",
+          titulo:"asdasdaqsdsssssssas",
+          autor:"coasdasd",
+          costo:"$ 20220",
+          ingredientes:"uasdas",
+          categoria:"Csadasd",
+          nivel:"asdasd", 
+        },
+
+      ]
         }
             
     }}
--- pastrycook/src/Views/BoxRecetas.vue
+++ pastrycook/src/Views/BoxRecetas.vue
@@ -1,29 +1,81 @@
 <template>
-    <div class="container flex mx-auto px-5 py-2 lg:px-32 lg:pt-12">
-            <div class="-m-1 flex flex-wrap md:-m-2" v-for="po in Pastry" v-bind:key="po.id">
-            <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../public/img/Pastel.png" alt="">
-                <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ po.titulo }}</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ po.descripcion }}</p>
+    <div>
+
+    <div class="flex justify-center items-center ">
+
+        <div class="container grid grid-rows-3  grid-cols-3 gap-10 my-6   flex justify-center items-center">
+            <div class="-m-1 flex flex-wrap md:-m-2" v-for="po in paginatedData" v-bind:key="po.id">
+                <a href="#" class="w-full flex flex-col items-center bg-white  border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../public/img/Pastel.png" alt="">
+                    <div class="flex flex-col justify-between p-4 leading-normal">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ po.titulo }}</h5>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ po.descripcion }}</p>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
-</a>
-</div>
-</div>
+
+    <div class="pagination">
+            <nav aria-label="Page navigation example">
+                <ul class="inline-flex -space-x-px text-sm">
+                    <li>
+                        <a href="#" @click="previousPage" :disabled="currentPage === 1" class="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+                    </li>
+                    <li v-for="page in totalPages" :key="page">
+                        <a href="#" @click="goToPage(page)" :class="{ 'text-blue-600 bg-blue-50': currentPage === page }"  class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{{ page }}</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#" @click="nextPage" :disabled="currentPage === totalPages" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    
+    </div>
 
 </template>
 
-    <script>
-    export default {
-        name: 'BoxRecetas',
-        props: {
-            msg: String,
-            Pastry: Array,
-            
 
-            
-        }
-       
-    }
-        
-    </script>
+
+<script>
+export default {
+    name: 'BoxRecetas',
+    props: {
+        msg: String,
+        Pastry: Array,
+    },
+    data() {
+        return {
+            currentPage: 1,
+            itemsPerPage: 6,
+        };
+    },
+    computed: {
+        totalPages() {
+            return Math.ceil(this.Pastry.length / this.itemsPerPage);
+        },
+        paginatedData() {
+            const start = (this.currentPage - 1) * this.itemsPerPage;
+            const end = start + this.itemsPerPage;
+            return this.Pastry.slice(start, end);
+        },
+    },
+    methods: {
+        nextPage() {
+            if (this.currentPage < this.totalPages) {
+                this.currentPage++;
+            }
+        },
+        previousPage() {
+            if (this.currentPage > 1) {
+                this.currentPage--;
+            }
+        },
+        goToPage(page) {
+            this.currentPage = page;
+        },
+    },
+};
+</script>
