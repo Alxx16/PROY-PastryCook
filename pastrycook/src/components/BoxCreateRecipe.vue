89518566795892471187669164544750ad1537b8
Alma Benítez <desarrollo@consentussg.com>
2023-07-24 15:08:50 -0500

    new Create

--- pastrycook/src/components/BoxCreateRecipe.vue
+++ pastrycook/src/components/BoxCreateRecipe.vue
@@ -2,17 +2,16 @@
     <div>
         <input v-model="titulo">
         <div>
-            <div class="flex items-center mb-4">
-                <input v-model="tipo" id="country-option-1" type="radio" name="countries" value="USA" class="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" checked>
-                <label for="country-option-1" class="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                Gratis
-                </label>
-            </div>
-
-            <div class="flex items-center mb-4">
-                <input v-model="tipo" id="country-option-2" type="radio" name="countries" value="Germany" class="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600">
-                <label for="country-option-2" class="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                Premium
+            <div v-for="plan in listPlanes" :key="plan.id_tipoPlan">
+                <input
+                    v-model="tipoPlanSeleccionado"
+                    :id="`plan-option-${plan.id_tipoPlan}`"
+                    type="radio"
+                    :name="`planes`"
+                    :value="plan.id_tipoPlan"
+                    class="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600">
+                <label :for="`plan-option-${plan.id_tipoPlan}`" class="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                    {{ plan.nombre_tipo }} 
                 </label>
             </div>
         </div>
@@ -21,7 +20,7 @@
         <input @change="selectFile" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" type="file">
         <label v-mode="autor"></label>
         <select v-model="categoria">
-            <option v-for="cat in categorias" :key="cat.id_P" :value="cat.id_P">{{ cat.postre }}</option>
+            <option v-for="cat in listCategoria" :key="cat.id_P" :value="cat.id_P">{{ cat.postre }}</option>
         </select>
         <input v-model="nivel">
         <input v-model="fecha">
@@ -49,22 +48,21 @@ export default {
             descripcion: '',
             autor: '',
             cate : '',
-            nivel: [],
+            listNivel: [],
             listCategoria: [],
-            planes: []
+            listPlanes: []
             
 
         }
     },
     async created() {
         const response = await get('http://localhost:3000/categorias?op=C');
-        //this.categorias = Array.from(response);
-        /*const tipo = await get('http://localhost:3000/categorias?op=D');
-        const planes = await get('http://localhost:3000/planes?op=C&idU=0&idP=0&desc=0');*/
+        const tipo = await get('http://localhost:3000/categorias?op=D');
+        const planes = await get('http://localhost:3000/planes?op=C&idU=0&idP=0&desc=0');
 
-        /*this.listCategoria = response;
-        this.planes = planes;
-        this.nivel = tipo;*/
+        this.listCategoria = response;
+        this.listPlanes = planes;
+        this.listNivel = tipo;
 
         console.log(response);
     
